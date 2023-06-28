fbbba3939df96592cfc6cf338fa6dcd32551d644
esha0612 <73080687+esha0612@users.noreply.github.com>
2023-06-28 12:37:40 -0700

    Update 200_stocksPreview.py

--- pages/200_stocksPreview.py
+++ pages/200_stocksPreview.py
@@ -1,12 +1,13 @@
 import streamlit as st
-import datetime,pytz
+import time,datetime,pytz,os,json
 import pandas as pd
+import streamlit as st
+import altair as alt
 from PIL import Image
-from timeplus import *
-import json
 
-st.set_page_config(layout="wide")
-col_img, col_txt, col_link = st.columns([1,8,5])
+from timeplus import *
+
+col_img, col_txt, col_link = st.columns([2,6,2])
 with col_img:
     image = Image.open("detailed-analysis@2x.png")
     st.image(image, width=100)
@@ -14,44 +15,107 @@ with col_txt:
     st.title("Real-time Insights for Stocks via Confluent Cloud")
 with col_link:
     st.markdown("[Source Code](https://github.com/esha0612/dashboard/edit/203dashboard/pages/200_stocksPreview.py)", unsafe_allow_html=True)
-    
-#env = Environment().address("https://us.timeplus.cloud").apikey("aV9q9Fz6uMhBK9TaGoh9iFdvowFnlVa3gavnoK8vEiSvKS1kHTo4YkxMDc2G").workspace("st3o6qm2")    
 
-MAX_ROW=10
-st.session_state.rows=0
-sql='SELECT _tp_time,symbol, userid, quantity, price, side FROM stocksEsha'
-st.code(sql, language="sql")
-with st.empty():
-    query = Query(env=env).sql(query=sql).create()
-    col = [h["name"] for h in query.metadata()["result"]["header"]]
-    def update_row(row,name):
-        data = {}
-        for i, f in enumerate(col):
-            data[f] = row[i]
-          
-
-        df = pd.DataFrame([data], columns=col)
-        st.session_state.rows=st.session_state.rows+1
-        if name not in st.session_state or st.session_state.rows>=MAX_ROW:
-            st.session_state[name] = st.table(df)
-            st.session_state.rows=0
-        else:
-            st.session_state[name].add_rows(df)
-    # iterate query result
-    limit = MAX_ROW*10-1
-    count = 0
-    for event in query.result():
+tab2, tab1 = st.tabs(["Realtime Data", "Event Count + Insights"])
+env = Environment().address("https://us.timeplus.cloud").apikey("aV9q9Fz6uMhBK9TaGoh9iFdvowFnlVa3gavnoK8vEiSvKS1kHTo4YkxMDc2G").workspace("st3o6qm2")    
+def batchQuery(bathSQL):
+    q=Query(env=env).sql(query=bathSQL).create()
+    header=q.metadata()["result"]["header"]
+    rows=[]
+    for event in q.result():
         if event.event != "metrics" and event.event != "query":
             for row in json.loads(event.data):
-                update_row(row,"tail")
-                count += 1
+                rows.append(row)
+    q.cancel()
+    q.delete()
+    return header,rows
+
+with tab2:
+    #st.header('New repos')
+    #show_table_for_query("""SELECT created_at,actor,repo,json_extract_string(payload,'master_branch') AS branch \nFROM github_events WHERE type='CreateEvent'""",'new_repo',3)
+
+    MAX_ROW=5
+    st.session_state.rows= 4
+    sql='SELECT _tp_time,symbol, userid, quantity, price, side FROM stocksEsha'
+    st.code(sql, language="sql")
+    with st.empty():
+        query = Query(env=env).sql(query=sql).create()
+        col = [h["name"] for h in query.metadata()["result"]["header"]]
+        def update_row(row,name):
+            data = {}
+            for i, f in enumerate(col):
+                data[f] = row[i]
+          
+
+            df = pd.DataFrame([data], columns=col)
+            st.session_state.rows=st.session_state.rows+1
+            if name not in st.session_state or st.session_state.rows>=MAX_ROW:
+                st.session_state[name] = st.table(df)
+                st.session_state.rows=0
+            else:
+                st.session_state[name].add_rows(df)
+    # iterate query result
+        limit = MAX_ROW*6-1
+        count = 0
+        for event in query.result():
+            if event.event != "metrics" and event.event != "query":
+                for row in json.loads(event.data):
+                    update_row(row,"tail")
+                    count += 1
+                    if count >= limit:
+                        break
+                # break the outer loop too    
+                if count >= limit:
+                    break                
+        query.cancel()
+        query.delete()
+
+    st.write(f"Only the recent {MAX_ROW*1} rows are shown. You can refresh the page to view the latest events.")
+with tab1:
+
+    st.header('New events every minute')
+    sql="SELECT window_end AS time,count() AS count from tumble(table(stocksEsha),1m) WHERE _tp_time > date_sub(now(), 2h) GROUP BY window_end"
+    st.code(sql, language="sql")
+    result=batchQuery(sql)
+    col = [h["name"] for h in result[0]]
+    df = pd.DataFrame(result[1], columns=col)
+    c = alt.Chart(df).mark_line(point=alt.OverlayMarkDef()).encode(x='time:T',y='count:Q',tooltip=['count'],color=alt.value('#D53C97'))
+    st.altair_chart(c, use_container_width=True)
+
+
+
+with tab1:
+
+    st.header('Event count')
+    st.code("SELECT count(*) FROM stocksEsha EMIT periodic 1s", language="sql")
+    with st.empty():
+        #show the initial events first
+        sql="select count(*) from table(stocksEsha)"
+        cnt=batchQuery(sql)[1][0][0]
+        st.metric(label="Stocks events", value="{:,}".format(cnt))
+        st.session_state.last_cnt=cnt
+
+        #create a streaming query to update counts
+        sql=f"select {cnt}+count(*) as events from stocksEsha emit periodic 1s"
+        query = Query(env=env).sql(query=sql).create()
+        def update_row(row):
+            delta=row[0]-st.session_state.last_cnt
+            if (delta>0):
+                st.metric(label="Stocks events", value="{:,}".format(row[0]), delta=row[0]-st.session_state.last_cnt, delta_color='inverse')
+                st.session_state.last_cnt=row[0]
+        # iterate query result
+        limit = 200
+        count = 0
+        for event in query.result():
+            if event.event != "metrics" and event.event != "query":
+                for row in json.loads(event.data):
+                    update_row(row)
+                    count += 1
+                    if count >= limit:
+                        break
+                # break the outer loop too    
                 if count >= limit:
                     break
-            # break the outer loop too    
-            if count >= limit:
-                break                
-    query.cancel()
-    query.delete()
-
-st.write(f"Only the recent {MAX_ROW*10} rows are shown. You can refresh the page to view the latest events.")
+        query.cancel()
+        query.delete()
 
